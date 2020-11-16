21fde3741ef3415ab75112271a56ca65686be4c7
Milos Vasic <milos85vasic@gmail.com>
2020-11-16 10:46:48 +0100

    MSF-343 - Parallels scripts - First touches.

--- run.sh
+++ run.sh
@@ -3,4 +3,16 @@
 cat logo.txt && \
 cat version.txt
 
-echo ""
+machine=$1
+echo "Checking system image availability"
+
+if test -e "$machine"; then
+
+  echo "$machine: Is ready"
+else
+
+  # TODO
+  echo "TODO"
+fi
+
+# TODO: Run machine
