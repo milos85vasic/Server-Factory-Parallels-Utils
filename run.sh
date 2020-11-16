e1405e7eb80467ee72c6764619d1161501951daa
Milos Vasic <milos85vasic@gmail.com>
2020-11-16 12:27:07 +0100

    MSF-343 - Working on parallels scripts.

--- run.sh
+++ run.sh
@@ -4,16 +4,26 @@ cat logo.txt && \
 cat version.txt
 echo ""
 
-machine=$1
+image=$1
 echo "Checking system image availability"
 
-if test -e "$machine"; then
+if test -e "$image"; then
 
-  echo "$machine: Is ready"
+  echo "$image: Is ready"
 else
 
-  # TODO
-  echo "TODO"
+  echo "$image: Is not available"
+  echo "$image: Obtaining"
+  image_location_settings="image_location.settings"
+  image_provider_settings="image_provider.settings"
+  if test -e "$image_location_settings"; then
+
+    image_location=$(cat "$image_location_settings")/Uncompressed
+  else
+
+    echo "ERROR: $image_location_settings not available, please create file and add absolute path to images to it"
+    exit 1
+  fi
 fi
 
 # TODO: Run machine
