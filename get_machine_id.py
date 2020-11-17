6bc3b71fc96e3b7deaf7e76a400d963c4f1069c5
Milos Vasic <milos85vasic@gmail.com>
2020-11-17 13:53:56 +0100

    MSF-343 - Obtaining machine ID.

--- get_machine_id.py
+++ get_machine_id.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python
+import sys
 
+
+def main():
+    if len(sys.argv) > 0:
+        image = sys.argv[0]
+
+    else:
+        print "No image path provided"
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
