ca2cd732817f61f42d9204cc164edba5897d625f
Milos Vasic <milos85vasic@gmail.com>
2020-11-17 14:02:59 +0100

    MSF-343 - Obtaining machine ID.

--- get_machine_id.py
+++ get_machine_id.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 import sys
+import subprocess
 
 
 def main():
-    if len(sys.argv) > 0:
-        image = sys.argv[0]
+    if len(sys.argv) > 1:
+        image = sys.argv[1]
+        output = subprocess.check_output(['prlctl', 'list', '-a', '-i'])
+        items = str(output).split("ID:")
+        for item in items:
+            if "Name:" in item:
+                rows = item.strip().split('\\n')
+                name = ""
+                for row in rows:
+                    if "Home:" in row and image in row:
+                        print("ROW: ", row)
 
     else:
-        print "No image path provided"
+        print("No image path provided")
         sys.exit(1)
 
 
