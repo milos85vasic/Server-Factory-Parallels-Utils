b54dacf0b1249f195148519390527e1f69bd98f7
Milos Vasic <milos85vasic@gmail.com>
2020-11-17 14:17:58 +0100

    MSF-343 - Obtaining machine ID - Completed.

--- get_machine_id.py
+++ get_machine_id.py
@@ -5,17 +5,33 @@ import subprocess
 
 def main():
     if len(sys.argv) > 1:
+        machines = dict()
         image = sys.argv[1]
+        row_home = "Home:"
+        row_id_open = "{"
+        row_id_close = "}"
         output = subprocess.check_output(['prlctl', 'list', '-a', '-i'])
         items = str(output).split("ID:")
         for item in items:
-            if "Name:" in item:
+            if row_home in item:
+                home = ""
+                machine_id = ""
                 rows = item.strip().split('\\n')
-                name = ""
                 for row in rows:
-                    if "Home:" in row and image in row:
-                        print("ROW: ", row)
+                    if row_id_open in row.strip() and row_id_close in row.strip():
+                        machine_id = row.replace(row_id_open, "").replace(row_id_close, "").strip()
+                    if row_home in row:
+                        home = row.replace(row_home, "").strip()
+                machines[home] = machine_id
 
+        for machine_id in machines:
+            if machine_id.startswith(image):
+                machine = machines[machine_id]
+                print(machine)
+                sys.exit(0)
+
+        print("Unknown_ID")
+        sys.exit(1)
     else:
         print("No image path provided")
         sys.exit(1)
